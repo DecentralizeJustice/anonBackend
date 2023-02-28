f5d60704dd045522bf5756d131fc10d32206893d
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-02-28 17:55:24 -0500

    sync new test

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -4,7 +4,7 @@ const handler = async (event) => {
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `He llo ${subject}` }),
+      body: JSON.stringify({ message: `He llo friend ${subject}` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
