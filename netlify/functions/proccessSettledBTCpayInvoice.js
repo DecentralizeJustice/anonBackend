d8b15c391f1d94b0d08f943a8a3b9ee9f7dbb4fc
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-02-28 18:26:59 -0500

    push new function

--- netlify/functions/proccessSettledBTCpayInvoice.js
+++ netlify/functions/proccessSettledBTCpayInvoice.js
@@ -1,15 +1,14 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+// /.netlify/functions/test
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    console.log(event.body)
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `He llo friend ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({ message: `Hello friend` })
     }
   } catch (error) {
+    console.log(error)
     return { statusCode: 500, body: error.toString() }
   }
 }
