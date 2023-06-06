13a7d279b1c3b74cfe1d015551845fe411b32751
Donely Gunn <32291941+itsMikeLowrey@users.noreply.github.com>
2023-06-06 10:50:42 -0400

    add max to all options

--- netlify/functions/proccessSettledBTCpayInvoice.js
+++ netlify/functions/proccessSettledBTCpayInvoice.js
@@ -138,7 +138,7 @@ async function processFirstAddressOrder(paymentInfo, invoiceId, params, client){
 async function sanatizeFirstAddressOrderInfo(orderInfo){
   const addressInfoSchema = Joi.object({
     country: Joi.string().required().min(0).max(99),
-    zipcode: Joi.required(),
+    zipcode: Joi.alternatives().try(Joi.string().min(0).max(99999), Joi.number().min(0).max(99999)).required(),
     city: Joi.string().required().min(0).max(99),
     streetAddress: Joi.string().required().min(0).max(999),
     fullname: Joi.string().required().min(0).max(99),
@@ -258,7 +258,7 @@ async function sanatizeFirstLockerOrderInfo(orderInfo){
     btcPayInvoice: Joi.string().required().alphanum().min(1).max(100),
     itemList: itemListSchema,
     country: Joi.string().required().min(0).max(99),
-    lockerZipcode: Joi.required(),
+    lockerZipcode: Joi.alternatives().try(Joi.string().min(0).max(99999), Joi.number().min(0).max(99999)).required(),
     lockerName: Joi.string().required().min(0).max(99),
     extraNotes: Joi.string().required().min(0).max(99999),
     type: Joi.string().required().min(0).max(50),
